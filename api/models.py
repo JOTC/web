c7a40223a838fc0bd4b591ba9c516119ec10ce59
Greg Walker <mgwalker@gmail.com>
2016-10-22 21:38:28 -0500

    Add first model; remove compiled files and caches

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Show(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=4096)
+    description = models.CharField(max_length=4096)
+    location = models.CharField(max_length=1024)
+    registration_deadline = models.DateTimeField()
+    registration_link = models.CharField(max_length=1024)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
